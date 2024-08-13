cb2c8a33e09d416a8c7ab618f005394345cc798f
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-08-13 22:46:21 +0500

    first

---
+++ backend-01/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+    res.send("rzn1337")
+});
+
+app.get('/login', (req, res) => {
+    res.send("<h1>sad</h1>")
+})
+
+app.get('/signup', (req, res) => {
+    res.send("<h2>Sign Up</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
